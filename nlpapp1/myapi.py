e142b4c4cf630ec71f69b4b8640e63d2f58e4d3d
bhumikashaw <bhumikashaw57@gmail.com>
2025-12-31 14:53:14 +0530

    myapi.py

--- nlpapp1/myapi.py
+++ nlpapp1/myapi.py
@@ -3,7 +3,7 @@ import nlpcloud
 class API:
 
     def __init__(self):
-        self.client = nlpcloud.Client("gpt-oss-120b", "0e8537908e5be6b8613e386ef6bbff2b31886338", gpu=True) #set your own key
+        self.client = nlpcloud.Client("gpt-oss-120b", "#Enter your own api key", gpu=True) #set your own key
 
     def sentiment_analysis(self,text):
         return self.client.sentiment(text)
@@ -12,4 +12,5 @@ class API:
         return self.client.entities(text,searched_entity)
     
     def language_detection(self,text):
+
         return self.client.langdetection(text)
